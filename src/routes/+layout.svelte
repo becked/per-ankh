a67760f13255cdda87fd80d5cc578d795fa2bf59
Jeff Beck <becked@fastmail.fm>
2025-11-22 11:55:04 -0500

    feat: add skeleton loading state for smoother page transitions
    - Add GamePageSkeleton component with matching layout to prevent shift
    - Disable ECharts animation for instant chart rendering
    - Remove fade transition from layout for faster navigation
    - Use minmax grid columns to stabilize summary section layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,15 @@
   import "../app.css";
   import Header from "$lib/Header.svelte";
   import GameSidebar from "$lib/GameSidebar.svelte";
-  import { page } from "$app/stores";
-  import { fade } from "svelte/transition";
 </script>
 
 <div class="flex flex-col h-screen overflow-hidden bg-blue-gray border-8 border-border-gray">
   <Header />
 
   <div class="flex flex-1 overflow-hidden">
-    {#key $page.url.pathname}
-      <div class="flex-1 flex flex-col min-w-0" in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
-        <slot />
-      </div>
-    {/key}
+    <div class="flex-1 flex flex-col min-w-0">
+      <slot />
+    </div>
     <GameSidebar />
   </div>
 </div>
